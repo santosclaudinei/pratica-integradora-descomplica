442952f07dc3d0b3f13ecc50fc26a64978c451cf
Claudinei José Santos <claudineibitta@gmail.com>
2024-12-08 23:13:55 -0300

    feat: arquivos referente ao app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import Header from "./components/header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <ListarTarefa />
+    </div>
+  );
+}
+
+export default App;
